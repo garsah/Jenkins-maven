78fd0829feb371d36768558fae60116b5fa6aa5e
jenkins <jenkins@localhost.localdomain>
2020-10-26 08:31:16 -0400

    Fixes workspace path for job execution

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,7 @@ echo "********************************************"
 echo "************** Testing The code*****************"
 echo "********************************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
